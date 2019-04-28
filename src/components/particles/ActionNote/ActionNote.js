ff26cd1b3410b690546f6b6e52e97b92e02a32e6
azerbelhedi <azerbelhedide@gmail.com>
2019-04-28 22:41:53 +0100

    start ActionNote

---
+++ src/components/particles/ActionNote/ActionNote.js
@@ -0,0 +1,14 @@
+import React , { Component } from 'react' ;
+import './ActionNote.css' ;
+
+class ActionNote extends Component{
+    render(){
+        return(
+            <div className = "action-note">
+                this is the action note box
+            </div>
+        )
+    }
+}
+
+export default ActionNote ;
